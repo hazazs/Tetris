b4a69fe23e13c5eda96e8e6838759f7911a82935
Tibor Hazafi <hazafi.tibor@gmail.com>
2021-11-29 22:10:36 +0100

    Tetris

--- src/hu/hazazs/tetris/TetrisGame.java
+++ src/hu/hazazs/tetris/TetrisGame.java
@@ -12,8 +12,6 @@ public final class TetrisGame implements Runnable {
 	}
 
 	// ██
-	// azt hiszem nagy nehezen sikerült
-	// én is úgy vélem Tibot, ügyes vagy
 	@Override
 	public void run() {
 		mainWindow.getGameArea().setText(level.toString());
